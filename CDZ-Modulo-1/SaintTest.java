22c77565f061dd7fdd7f264828981fc4b3e951f2
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-04-13 17:43:32 -0300

    test(Modulo-1): Criar teste aoCriarSaintGeneroENaoInformado na classe SaintTest

--- CDZ-Modulo-1/SaintTest.java
+++ CDZ-Modulo-1/SaintTest.java
@@ -10,7 +10,7 @@ public class SaintTest {
     public void vestirArmaduraDeixaArmaduraVestida() {
         // AAA
         // 1. Arrange - Montagem do cenario de teste 
-        Armadura touro = new Armadura("Touro");
+        Armadura touro = new Armadura("Touro", Categoria.OURO);
         Saint jubileu = new Saint("Jubileu", touro);
         
         // 2. Action - Invocar a ação a ser testada       
@@ -22,7 +22,13 @@ public class SaintTest {
     
     @Test
     public void vestirArmaduraDeixaArmaduraNaoVestida() {
-        Saint jubileu = new Saint("Jubileu", new Armadura("Touro"));
+        Saint jubileu = new Saint("Jubileu", new Armadura("Cisnei", Categoria.BRONZE));
         assertEquals(false, jubileu.isArmaduraVestida());
+    }
+    
+    @Test
+    public void aoCriarSaintGeneroENaoInformado() {
+        Saint saint = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
+        assertEquals(Genero.NAO_INFORMADO, saint.getGenero());
     }
 }
