6d4910d8180c1923cbab976cd38dfa0d07ccdbf3
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-04-13 17:42:04 -0300

    feat(Modulo-1): Criar ENUM Genero e adicionar atributo genero na classe Saint

--- CDZ-Modulo-1/Saint.java
+++ CDZ-Modulo-1/Saint.java
@@ -3,10 +3,12 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
+    private Genero genero;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
         this.armadura = armadura;
+        this.genero = Genero.NAO_INFORMADO;
     }
     
     public void vestirArmadura() {
@@ -16,4 +18,12 @@ public class Saint {
     public boolean isArmaduraVestida() {
         return armaduraVestida;
     }
+    
+    public Genero getGenero() {
+        return genero;
+    }
+    
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    } 
 }
