6b4de5b76b0785c8b0a826a5fc213ffa52332b6f
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-13 23:40:01 -0300

    feat(Modulo-1 Lista 1): Adicionar atributos de Status e quantidade de hp na classe Saint

--- CDZ-Modulo-1/Saint.java
+++ CDZ-Modulo-1/Saint.java
@@ -4,11 +4,15 @@ public class Saint {
     private Armadura armadura;
     private boolean armaduraVestida;
     private Genero genero;
+    private Status status;
+    private double hp;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
         this.armadura = armadura;
         this.genero = Genero.NAO_INFORMADO;
+        this.status = Status.VIVO;
+        hp = 100;
     }
     
     public void vestirArmadura() {
