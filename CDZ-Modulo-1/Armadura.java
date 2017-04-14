2f61fbcbee45029ff0700e5b84a7e750b4585ffe
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-14 10:30:03 -0300

    fix(Modulo-1 Lista 1): Corrigir nome de parametro no Construtor da Armadura

--- CDZ-Modulo-1/Armadura.java
+++ CDZ-Modulo-1/Armadura.java
@@ -3,7 +3,7 @@ public class Armadura {
     private String constelacao;
     private Categoria categoria;
     
-    public Armadura(String constelacao, Categoria categotia) {
+    public Armadura(String constelacao, Categoria categoria) {
         this.constelacao = constelacao;
         this.categoria = categoria;
     }
