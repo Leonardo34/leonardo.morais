03923b05ee1c2a2a0dad730cb9e8a7e11a98957c
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-14 09:52:18 -0300

    feat(Modulo-1 Lista 1): Modelar a classe Batalha

---
+++ CDZ-Modulo-1/Batalha.java
@@ -0,0 +1,9 @@
+
+public class Batalha {
+    private Saint saintUm, saintDois;
+    
+    public Batalha(Saint saintUm, Saint saintDois) {
+        this.saintUm = saintUm;
+        this.saintDois = saintDois;
+    }
+}
