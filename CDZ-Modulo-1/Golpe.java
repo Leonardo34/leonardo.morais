bdbdfb66658699ae3676d57e0ca17b6ff22d11de
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-17 18:34:19 -0300

    feat(Modulo-1 Golpe): Modelar classe Golpe

---
+++ CDZ-Modulo-1/Golpe.java
@@ -0,0 +1,18 @@
+
+public class Golpe {
+    private String nome;
+    private int fatorDano;
+    
+    public Golpe(String nome, int fatorDano) {
+        this.nome = nome;
+        this.fatorDano = fatorDano;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getFatorDano() {
+        return fatorDano;
+    }
+}
---
+++ CDZ-Modulo-1/GolpeTest.java
@@ -0,0 +1,16 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GolpeTest {
+    
+    @Test
+    public void testarConstrucaoObjetoGolpe() {
+        Golpe golpe = new Golpe("kamehameha", 8001);
+        assertEquals("kamehameha", golpe.getNome());
+        assertEquals(8001, golpe.getFatorDano());
+    }
+}
