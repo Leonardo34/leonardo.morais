181b23834191809d17e038a1d31675dd8fa3c4bc
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-04-18 15:38:59 -0300

    feat(Modulo-1 Golpe): Sobrescrever equals

--- CDZ-Modulo-1/Golpe.java
+++ CDZ-Modulo-1/Golpe.java
@@ -2,17 +2,32 @@
 public class Golpe {
     private String nome;
     private int fatorDano;
-    
+
     public Golpe(String nome, int fatorDano) {
         this.nome = nome;
         this.fatorDano = fatorDano;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public int getFatorDano() {
         return fatorDano;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this.getClass() != object.getClass()) {
+            return false;
+        }
+        Golpe other = (Golpe) object;
+        if (this.nome != other.nome) {
+            return false;
+        }
+        if (this.fatorDano != other.fatorDano) {
+            return false;
+        }
+        return true;
     }
 }
