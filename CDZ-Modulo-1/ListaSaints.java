3cdb846a22354f45cb621dfe5d56dcb83b8cf1f9
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-04-18 17:27:30 -0300

    feat(Modulo-1 ListaSaints): Iniciar implementação da classe para armazenar um Conjunto de Saints

---
+++ CDZ-Modulo-1/ListaSaints.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaSaints {
+    private List<Saint> saints;
+    
+    public ListaSaints() {
+        this.saints = new ArrayList<>();
+    }
+}
