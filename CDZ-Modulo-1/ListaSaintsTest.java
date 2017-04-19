72890b4365ae452215c62a62d97d7bf19fc5fcfd
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-19 00:09:58 -0300

    test(Modulo-1 ListaSaints): Garantir retorno correto ao buscar Saint por nome

--- CDZ-Modulo-1/ListaSaintsTest.java
+++ CDZ-Modulo-1/ListaSaintsTest.java
@@ -11,4 +11,16 @@ public class ListaSaintsTest {
         ListaSaints lista = new ListaSaints();
         assertEquals(null, lista.buscarPorNome("Zé"));
     }
+    
+    @Test
+    public void buscarPorNomeSaintInseridoRetornaSaint() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint saint = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+        Saint shuka = new GoldSaint("Shuka", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+        lista.adicionar(saint);
+        lista.adicionar(shuka);
+        assertEquals(saint, lista.buscarPorNome("Shaka"));
+        assertEquals(shuka, lista.buscarPorNome("Shuka"));
+        assertEquals(null, lista.buscarPorNome("Zé"));
+    }
 }
