766d58927d0eb4e663c7645af0d2a758893a202a
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-19 00:33:15 -0300

    test(Modulo-1 ListaSaints): Implementar testes para o método buscaPorCategoria

--- CDZ-Modulo-1/ListaSaintsTest.java
+++ CDZ-Modulo-1/ListaSaintsTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
 
 public class ListaSaintsTest {
     
@@ -37,5 +38,31 @@ public class ListaSaintsTest {
         assertEquals(shuka, lista.buscarPorNome("Shuka"));
         assertEquals(null, lista.buscarPorNome("Zé"));
         assertEquals(saint, lista.buscarPorNome("Shaka"));
+    }
+    
+    @Test
+    public void buscarSaintPorCategoriaSemNenhumMenbroListaVazia() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint saint = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+        Saint saintDois = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.PRATA));
+        lista.adicionar(saint);
+        lista.adicionar(saintDois);
+        assertEquals(true, lista.buscarPorCategoria(Categoria.BRONZE).isEmpty());
+    }
+    
+    @Test
+    public void adicionarDoisSaintsOuroBuscarPorOuro() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint saintOuroUm = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+        Saint saintPrata = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.PRATA));
+        Saint saintOuroDois = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.OURO));
+        Saint saintBronze = new GoldSaint("Shaka", new Armadura(new Constelacao("Touro"), Categoria.BRONZE));
+        lista.adicionar(saintOuroUm);
+        lista.adicionar(saintPrata);
+        lista.adicionar(saintBronze);
+        lista.adicionar(saintOuroDois);
+        List<Saint> listaSaintsOuro = lista.buscarPorCategoria(Categoria.OURO);
+        assertEquals(saintOuroUm, listaSaintsOuro.get(0));
+        assertEquals(saintOuroDois, listaSaintsOuro.get(1));
     }
 }
