145a04734e939b4aea4c4c29dd57eaf083260279
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-22 17:41:38 -0300

    test(Modulo-1 Golpear): Adicionar casos de teste para SilverSaints

--- CDZ-Modulo-1/GolpearTest.java
+++ CDZ-Modulo-1/GolpearTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class GolpearTest {
     
     @Test
-    public void GolpearComGoldSaintSemArmadura() throws Exception {
+    public void golpearComGoldSaintSemArmadura() throws Exception {
         Saint saint = new GoldSaint("Goku", "Touro");
         saint.aprenderGolpe(new Golpe("Kamehameha", 10));
         Saint saintApanha = new BronzeSaint("Seiya", "Pegasus");
@@ -20,7 +20,7 @@ public class GolpearTest {
     }
     
     @Test
-    public void GolpearComGoldSaintComArmaduraVestida() throws Exception {
+    public void golpearComGoldSaintComArmaduraVestida() throws Exception {
         Saint saint = new GoldSaint("Goku", "Touro");
         saint.aprenderGolpe(new Golpe("Kamehameha", 10));
         Saint saintApanha = new BronzeSaint("Seiya", "Pegasus");
@@ -34,5 +34,48 @@ public class GolpearTest {
         assertEquals(82, saintApanha.getVida(), 0);
         movimento.executar();
         assertEquals(42, saintApanha.getVida(), 0);
+    }
+    
+    @Test
+    public void golpearComSaintSemGolpes() throws Exception {
+        Saint saint = new GoldSaint("Goku", "Touro");
+        Saint saintApanha = new BronzeSaint("Seiya", "Pegasus");
+        Movimento movimento = new Golpear(saint, saintApanha);
+        movimento.executar();
+        assertEquals(100, saintApanha.getVida(), 0);
+    }
+    
+    @Test
+    public void golpearComSaintPrataSemArmadura() {
+        Saint saint = new SilverSaint("Goku", "Touro");
+        saint.aprenderGolpe(new Golpe("Kamehameha", 20));
+        Saint saintApanha = new BronzeSaint("Seiya", "Pegasus");
+        Movimento movimento = new Golpear(saint, saintApanha);
+        movimento.executar();
+        assertEquals(80, saintApanha.getVida(), 0);
+        movimento.executar();
+        assertEquals(60, saintApanha.getVida(), 0);
+        saint.aprenderGolpe(new Golpe("Soco", 5));
+        movimento.executar();
+        assertEquals(55, saintApanha.getVida(), 0);
+    }
+    
+    @Test
+    public void golpearComSaintPrataComArmadura() {
+        Saint saint = new SilverSaint("Goku", "Touro");
+        saint.aprenderGolpe(new Golpe("Kamehameha", 20));
+        Saint saintApanha = new BronzeSaint("Seiya", "Pegasus");
+        Movimento movimento = new Golpear(saint, saintApanha);
+        Movimento vestirArmadura = new VestirArmadura(saint);
+        movimento.executar();
+        assertEquals(80, saintApanha.getVida(), 0);
+        vestirArmadura.executar();
+        movimento.executar();
+        assertEquals(20, saintApanha.getVida(), 0);
+        saint.aprenderGolpe(new Golpe("Rasteira", 5));
+        movimento.executar();
+        assertEquals(5, saintApanha.getVida(), 0);
+        movimento.executar();
+        assertEquals(Status.MORTO, saintApanha.getStatus());
     }
 }
