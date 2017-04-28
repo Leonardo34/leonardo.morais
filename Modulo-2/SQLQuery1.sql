d48141ffc94eb5d5dbaca0d3e30edffba21672c4
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-04-28 15:11:50 -0300

    feat(Modulo-2 Exercicio-1): Adicionar campo Preço aos Produtos

--- Modulo-2/SQLQuery1.sql
+++ Modulo-2/SQLQuery1.sql
@@ -17,17 +17,18 @@ Create table Produto (
 	NomeCurto varchar(10) not null,
 	NomeDescritivo varchar(40) not null,
 	DataCriacao datetime not null,
-	LocalEstoque varchar(50),
-	Quantidade int,
+	LocalEstoque varchar(50) not null,
+	Quantidade int not null,
+	Preco decimal(9, 2) not null,
 		constraint PK_Produto primary key (IDProduto)
 );
 
 --Exercicio 4
 
-insert into Produto (IDProduto, NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade)
-	values (1, 'Café', 'Café Loco', convert(datetime, '1980/12/17', 111), 'Céu', 8001);
+insert into Produto (IDProduto, NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade, Preco)
+	values (1, 'Café', 'Café Loco', convert(datetime, '1980/12/17', 111), 'Céu', 8001, 500.49);
 
-insert into Produto (IDProduto, NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade)
-	values (2, 'Chocolate', 'Chocolate meio amargo', convert(datetime, '2017/01/01', 111), 'Armário', 10);
+insert into Produto (IDProduto, NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade, Preco)
+	values (2, 'Chocolate', 'Chocolate meio amargo', convert(datetime, '2017/01/01', 111), 'Armário', 10, 5.99);
 
 select * from Produto;
