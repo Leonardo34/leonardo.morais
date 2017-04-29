2955857aa8d0ceace2a33e9ffe31c37bd5a88f2e
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-29 11:40:44 -0300

    feat(Modulo-1 ContraAtaque): Receber sorteador para sortear se o contra-ataque será efetivamente realizado

--- CDZ-Modulo-1/ContraAtaque.java
+++ CDZ-Modulo-1/ContraAtaque.java
@@ -1,14 +1,17 @@
 
 public class ContraAtaque implements Movimento {
     private Saint golpeador, golpeado;
+    private Sorteador sorteador;
     
-    public ContraAtaque(Saint golpeador, Saint golpeado) {
+    public ContraAtaque(Saint golpeador, Saint golpeado, Sorteador sorteador) {
         this.golpeador = golpeador;
         this.golpeado = golpeado;
+        this.sorteador = sorteador;
     }
     
     @Override
     public void executar() {
+        
     }
     
     @Override
