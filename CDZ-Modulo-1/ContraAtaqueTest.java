af2c4129a96828299a892c79120627e7a62aba86
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-04-29 14:04:36 -0300

    test(Modulo-1 ContraAtaque): Implementar casos de teste para garantir que o contra-ataque somente acontece se o saint possui menos de 50 de vida e está sem armadura

--- CDZ-Modulo-1/ContraAtaqueTest.java
+++ CDZ-Modulo-1/ContraAtaqueTest.java
@@ -25,4 +25,47 @@ public class ContraAtaqueTest {
         assertEquals(40, golpeado.getVida(), 0);
         assertEquals(75, golpeador.getVida(), 0);
     }
+    
+    @Test
+    public void contraAtaqueComArmaduraVestidaNaoAcontece() {
+        Saint golpeado = new BronzeSaint("Seiya", "Pegasus");
+        Saint golpeador = new SilverSaint("Naruto", "Konoha");
+        Movimento contraAtaque = new ContraAtaque(golpeador, golpeado, new Sorteador() {
+            @Override
+            public int sortear() {
+                return 1;
+            }
+        });
+        golpeado.perderVida(70);
+        golpeado.vestirArmadura();
+        contraAtaque.executar();
+        golpeado.perderVida(10, golpeador);
+        assertEquals(20, golpeado.getVida(), 0);
+        assertEquals(100, golpeador.getVida(), 0);
+    }
+    
+    @Test
+    public void contraAtacarSomenteFuncionaComVidaMenor50EArmaduraNaoVestida() {
+        Saint golpeado = new BronzeSaint("Seiya", "Pegasus");
+        Saint golpeador = new SilverSaint("Naruto", "Konoha");
+        Movimento contraAtaque = new ContraAtaque(golpeador, golpeado, new Sorteador() {
+            @Override
+            public int sortear() {
+                return 1;
+            }
+        });
+        contraAtaque.executar();
+        golpeado.perderVida(60);
+        assertEquals(40, golpeado.getVida(), 0);
+        assertEquals(100, golpeador.getVida(), 0);
+        contraAtaque.executar();
+        golpeado.perderVida(10, golpeador);
+        assertEquals(40, golpeado.getVida(), 0);
+        assertEquals(75, golpeador.getVida(), 0);
+        golpeado.vestirArmadura();
+        contraAtaque.executar();
+        golpeado.perderVida(10, golpeador);
+        assertEquals(30, golpeado.getVida(), 0);
+        assertEquals(75, golpeador.getVida(), 0);
+    }
 }
