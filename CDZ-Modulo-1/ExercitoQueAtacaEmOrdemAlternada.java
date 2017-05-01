d9f47efd3a4313f5286ff836e4a0d4543542edd0
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-01 18:22:06 -0300

    feat(Modulo-1 ExercitoQueAtacaEmOrdemAlternada): Modelar classe para exercito que manda seus Saints para a batalha de forma cíclica
    Exemplo (exército com 6 Saints, 2 de cada tipo):
    Bronze1 vai pra batalha
    Prata1 vai pra batalha
    Ouro1 vai pra batalha
    Bronze2 vai pra batalha
    Prata2 vai pra batalha
    Ouro2 vai pra batalha

---
+++ CDZ-Modulo-1/ExercitoQueAtacaEmOrdemAlternada.java
@@ -0,0 +1,8 @@
+
+public class ExercitoQueAtacaEmOrdemAlternada extends ExercitoDeSaints {
+    
+    @Override
+    public Saint getProximoSaint() throws ExercitoVazioException {
+        return null;
+    }
+}
