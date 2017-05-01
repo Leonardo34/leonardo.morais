fd299292b6e5117a973f5f09ddf61cb95041c731
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-01 15:40:32 -0300

    feat(Modulo-1 ExercitoDeSaints): Adicionar classe abstrata representando exercito de Saints

---
+++ CDZ-Modulo-1/ExercitoDeSaints.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class ExercitoDeSaints {
+    protected List<Saint> exercito;
+    
+    public ExercitoDeSaints() {
+        exercito = new ArrayList<>();
+    }
+    
+    public void alistar(Saint saint) {
+        exercito.add(saint);
+    }
+    
+    public boolean estaVazio() {
+        return exercito.isEmpty();
+    }
+}
