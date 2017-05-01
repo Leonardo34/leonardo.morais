93788f12f44b90be3533b3e6cbe13c89467f9232
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-01 18:44:34 -0300

    refactor(Modulo-1 Golpear): Passar golpeador como parametro para o método perderVida para que o saint possa contra-atacar, caso esse movimento esteja ativado

--- CDZ-Modulo-1/Golpear.java
+++ CDZ-Modulo-1/Golpear.java
@@ -9,10 +9,9 @@ public class Golpear implements Movimento {
     
     @Override
     public void executar() {
-        int multiploDano = golpeador.getMultiploDano();
         try {
-            double vidaPerder = golpeador.getProximoGolpe().getFatorDano() * multiploDano;
-            golpeado.perderVida(vidaPerder);
+            double vidaPerder = golpeador.getProximoGolpe().getFatorDano() * golpeador.getMultiploDano();
+            golpeado.perderVida(vidaPerder, golpeador);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
