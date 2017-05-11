6970e2a90d3fc2991cfce6eaa7e51825e3231c3c
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-05-11 17:20:19 -0300

    feat(Modulo-3 Exercicio Inicial): Adicionar demostração de formas de definição de funções

--- Modulo-4/Aula-1/js/start.js
+++ Modulo-4/Aula-1/js/start.js
@@ -1,5 +1,16 @@
 function soma(a, b) {
     return a + b;
-}
+};
+console.log(soma(5, 5));
+
+var somaDois = function(a, b) {
+    return a + b;
+};
+console.log(somaDois(5, 5));
+
+var somaTres = (a, b) => a + b
+console.log(somaTres(5, 5));
+
+console.log(somaTres(5, 5, 5));
 
 console.log('carregou');
