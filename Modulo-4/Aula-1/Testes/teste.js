d15053fd1abdf3736c93e58d4c35567557444980
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-12 01:24:19 -0300

    test(Modulo-4 Lista-1): Adicionar testes para a função que encontra o maior texto em um array

--- Modulo-4/Aula-1/Testes/teste.js
+++ Modulo-4/Aula-1/Testes/teste.js
@@ -2,4 +2,13 @@ describe('Teste de daisyGame', function() {
     it('daisyGame com numero par', function() {
         expect(daisyGame(6)).toEqual("Love Me Not");
     });
+    it('daisyGame com numero impar', function() {
+        expect(daisyGame(5)).toEqual("Love Me");
+    });
 });
+
+describe('Teste do maior texto em um array', function() {
+    it('MaiorTexto deve retornar a maior string do array', function() {
+        expect(maiorTexto(["Leonardo", "Leozin", "AAA"])).toEqual("Leonardo");
+    });
+})
