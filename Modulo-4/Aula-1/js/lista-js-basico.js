cbb5cfb69373bb4455410a0b131ca05db56029aa
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-12 00:10:27 -0300

    feat(Modulo-4 Lista-1): Adicionar resolução dos exercicios 2 e 3

--- Modulo-4/Aula-1/js/lista-js-basico.js
+++ Modulo-4/Aula-1/js/lista-js-basico.js
@@ -1,4 +1,4 @@
-var daisyGame = function (petalas) {
+var daisyGame = function(petalas) {
     if (petalas % 2) {
         return "Love Me";
     }
@@ -6,3 +6,26 @@ var daisyGame = function (petalas) {
 };
 console.log(daisyGame(4));
 console.log(daisyGame(3));
+
+var maiorTexto = function(textos) {
+    var maiorPalavra = "";
+    for (var texto of textos) {
+        if (texto.length > maiorPalavra.length) {
+            maiorPalavra = texto;
+        }
+    }
+    return maiorPalavra;
+};
+console.log(maiorTexto(["Leonardo", "Morais", "Broch", "Leo", "Leozin"]));
+console.log(maiorTexto(["NaN", "Not", "AAAA"]));
+
+var imprimir = function (nomes, callback) {
+    for (var nome of nomes) {
+        callback(nome);
+    }
+};
+imprimir(
+    ['bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos'], function(instrutor) {
+        console.log("Ola querido instrutor: ", instrutor);
+    }
+);
