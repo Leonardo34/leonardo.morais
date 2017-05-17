3214de9b7e8008080b6fd61001e87a0e7dc54e40
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-05-17 15:52:09 -0300

    feat(Modulo-5 Exercicio-Aula-1): Adicionar exercicio inicial com o framework AngularJS

--- Modulo-5/Aula-1/js/controller.js
+++ Modulo-5/Aula-1/js/controller.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('tutorialAngularJS', []);
 
 myApp.controller('TutorialController', function($scope) {
-  $scope.yourName = "seila";
+    $scope.pokemon = {nome: "Pikachu", id: 25};
 });
