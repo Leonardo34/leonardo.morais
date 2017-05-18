4cc03527809aa7e3b6736abe75c8476f496e500f
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-05-18 17:09:07 -0300

    feat(Modulo-5 Exercicio Filter): Adicionar resolução do exercicio para criar filtros com angular

---
+++ Modulo-5/Aula-2/js/controllerExemplo2.js
@@ -0,0 +1,47 @@
+var myApp = angular.module('exemplo02App', []);
+
+myApp.filter('mascada', function() {
+    return function(input) {
+        return input.toLowerCase() == "nunes" ? "$ " + input + " $" : input;
+    }
+});
+
+myApp.controller('exemplo02Controller', function($scope) {
+
+    let instrutores = [
+    {
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+        numero: 4,
+        nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+            numero: 2,
+            nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+            numero: 3,
+            nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+            numero: 5,
+            nome: 'AngularJS'
+        }]
+    }];
+
+    $scope.instrutores = instrutores;
+});
