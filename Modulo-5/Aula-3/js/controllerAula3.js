87a8e121922bde68a570552b71149cf7779fce0a
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-05-19 15:16:22 -0300

    feat(Modulo-5 Aula-3): Adicionar formulário para permitir cadastrar novas aulas

---
+++ Modulo-5/Aula-3/js/controllerAula3.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('exemplo02App', []);
+
+myApp.filter('mascada', function() {
+    return function(input) {
+        return input.toLowerCase() == "nunes" ? "$ " + input + " $" : input;
+    }
+});
+
+myApp.filter('aula', function() {
+    return function(input) {
+        return "00" + input.numero + " - " + input.nome.toUpperCase();
+    }
+});
+
+myApp.controller('exemplo02Controller', function($scope) {
+
+    let instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+            numero: 2,
+            nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+            numero: 3,
+            nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+            numero: 5,
+            nome: 'AngularJS'
+        }]
+    }];
+
+    $scope.aulas = [];
+    instrutores.forEach(instrutor => {
+        instrutor.aula.forEach(a => {
+            $scope.aulas.push({instrutor: instrutor.nome, numero: a.numero, nome: a.nome})
+        })
+    });
+
+    $scope.update = (aula) => {
+        $scope.aulas.push(aula);
+    };
+});
