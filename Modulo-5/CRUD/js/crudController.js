1b01d1fcba0ecad59d8aa21f5ac143e4e406fc91
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-20 11:55:07 -0300

    feat(Modulo-5 CRUD): Permitir adição de novas aulas e remover aula já existente

--- Modulo-5/CRUD/js/crudController.js
+++ Modulo-5/CRUD/js/crudController.js
@@ -1,7 +1,20 @@
 var myApp = angular.module('crudApp', []);
 
 myApp.controller('crudController', function($scope) {
+    let idGenerator = 0;
 
     $scope.aulas = [];
 
+    $scope.update = (aula) => {
+        if ($scope.cadastroInstrutor.$valid) {
+            aula.id = idGenerator++;
+            $scope.aulas.push(aula);
+            delete $scope.aula;
+        }
+    }
+
+    $scope.remove = (aula) => {
+        let indice = $scope.aulas.indexOf(aula);
+        $scope.aulas.splice(indice, 1);
+    }
 });
