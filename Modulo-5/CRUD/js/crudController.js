45f485857547f9d4cde893948674105e63d2c6e2
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-20 14:06:04 -0300

    feat(Modulo-5 CRUD): Impedir que usuário possa cadastrar aulas com mesmo nome

--- Modulo-5/CRUD/js/crudController.js
+++ Modulo-5/CRUD/js/crudController.js
@@ -5,24 +5,33 @@ myApp.controller('crudController', function($scope) {
 
     $scope.aulas = [];
 
-    $scope.update = (aula) => {
-        if ($scope.cadastroAula.$valid) {
+    $scope.saveAula = (aula) => {
+        if ($scope.cadastroAula.$valid && !existeAulaComNome(aula.nome)) {
             aula.id = idGenerator++;
             $scope.aulas.push(aula);
             delete $scope.aula;
         }
     }
 
-    $scope.remove = (aula) => {
+    $scope.removeAula = (aula) => {
         let indice = $scope.aulas.indexOf(aula);
         $scope.aulas.splice(indice, 1);
     }
 
-    $scope.alter = (aula) => {
-        if ($scope.alterarAula.$valid) {
-            let index = $scope.aulas.findIndex(a => a.id === parseInt(aula.id));
-            $scope.aulas[index].nome = aula.nome;
+    $scope.updateAula = (aula) => {
+        if ($scope.alterarAula.$valid && !existeAulaComNome(aula.nome)) {
+            let aulaUpdate = getAulaById(parseInt(aula.id));
+            aulaUpdate.nome = aula.nome;
             delete $scope.edit;
         }
+    }
+
+    var getAulaById = (id) => {
+        let index = $scope.aulas.findIndex(a => a.id === id);
+        return $scope.aulas[index];
+    }
+
+    var existeAulaComNome = (nome) => {
+        return $scope.aulas.some(a => a.nome === nome);
     }
 });
