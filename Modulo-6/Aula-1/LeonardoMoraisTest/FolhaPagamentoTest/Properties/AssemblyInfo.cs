192e05e7092cf9838f705d74c1badcded152e4e5
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-25 10:31:14 -0300

    feat(Modulo-6 Exercicio-1): Adicionar testes unitários e ajustar arredondamento dos valores calculados

---
+++ Modulo-6/Aula-1/LeonardoMoraisTest/FolhaPagamentoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FolhaPagamentoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FolhaPagamentoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a587f679-3ba6-4215-a86e-7869ef85fe02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
