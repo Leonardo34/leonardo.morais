b68e238195a8ac04adc31c9f131726c795b3b5e5
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-27 20:55:11 -0300

    feat(Modulo-6 Chat): Adicionar estrutura inicial da API e template html do ZapZapMessenger

---
+++ Modulo-6/Zap-Zap/ChatAPI/ChatAPI/Controllers/MensagensController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatAPI.Controllers
+{
+    public class MensagensController : ApiController
+    {
+        public string Get()
+        {
+            return "Ola";
+        }
+    }
+}
---
+++ Modulo-6/Zap-Zap/ChatAPI/ChatAPI/Models/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatAPI.Models
+{
+    public class Mensagem
+    {
+        public string NomeAutor { get; set; }
+        public string UrlFotoAutor { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
