b4ac40bc0c286a0d8864f9db15c68dc21d48aebd
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-30 01:26:10 -0300

    feat(Modulo-6 Aula-4): Adicionar métodos para alterar, excluir e buscar produtos pelo Id

--- Modulo-6/Aula-4/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
+++ Modulo-6/Aula-4/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -63,5 +63,65 @@ namespace Demo1.Infraestrutura.Repositorios
             }
             return produtos;
         }
+
+        public void Alterar(Produto produto)
+        {
+            using (var conexaoDb = new SqlConnection(CONEXAO_STRING))
+            {
+                conexaoDb.Open();
+                using (var command = conexaoDb.CreateCommand())
+                {
+                    command.CommandText =
+                        "UPDATE PRODUTO SET Nome = @nome, Preco = @preco, Estoque = @estoque WHERE Id = @id";
+                    command.Parameters.AddWithValue("nome", produto.Nome);
+                    command.Parameters.AddWithValue("preco", produto.Preco);
+                    command.Parameters.AddWithValue("estoque", produto.Estoque);
+                    command.Parameters.AddWithValue("id", produto.Id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var conexaoDb = new SqlConnection(CONEXAO_STRING))
+            {
+                conexaoDb.Open();
+                using (var command = conexaoDb.CreateCommand())
+                {
+                    command.CommandText = "DELETE Produto WHERE Id = @id";
+                    command.Parameters.AddWithValue("id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Produto Obter(int id)
+        {
+            Produto produto = null;
+            using (var conexaoDb = new SqlConnection(CONEXAO_STRING))
+            {
+                conexaoDb.Open();
+                using (var comando = conexaoDb.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, Nome, Preco, Estoque FROM Produto WHERE Id = @id";
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        produto = new Produto();
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+                        return produto;
+                    }
+                }
+            }
+            return produto;
+        }
     }
 }
