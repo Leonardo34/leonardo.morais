485b250e8ca862c294d4445f3c576483d6dce8a5
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-05-30 17:30:56 -0300

    feat(Modulo-6 Aula-5): Adicionar arquivos do projeto EditoraCrescer

---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Contexto/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Contexto
+{
+    public class Contexto : DbContext 
+    {
+        public Contexto() : base("Server=191.232.191.228; Database=aluno04db; User id=leonardo.morais; Password=123456")
+        { }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AutorMap());
+            modelBuilder.Configurations.Add(new LivroMap());
+        }
+    }
+}
---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EditoraCrescer.Infraestrutura.Contexto;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+    public class LivroRepositorio
+    {
+        private Contexto.Contexto livroContexto = new Contexto.Contexto();
+
+        public LivroRepositorio()
+        { }
+
+        public List<Livro> Listar()
+        {
+            return livroContexto.Livros.ToList();
+        }
+    }
+}
