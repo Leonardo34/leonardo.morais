3c3488621f76dde27ece230da9853cf2b84190e6
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-30 08:49:05 -0300

    feat(Modulo-6 Aula-4): Adicionar controller para pedidos, podendo adicionar novo pedido e alterar pedido existente

---
+++ Modulo-6/Aula-4/Demo1/Demo1WebAPI/Controllers/PedidosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Demo1.Infraestrutura.Repositorios;
+using Demo1.Dominio.Entidades;
+
+namespace Demo1WebAPI.Controllers
+{
+    public class PedidosController : ApiController
+    {
+        private PedidoRepositorio repositorio = new PedidoRepositorio();
+
+        public IHttpActionResult Post(Pedido pedido)
+        {
+            List<string> listaErros;
+            if (!pedido.Validar(out listaErros))
+            {
+                return BadRequest(string.Join(".\n", listaErros.ToArray()));
+            }
+            repositorio.Criar(pedido);
+            return Ok(pedido);
+        }
+
+        public IHttpActionResult Put(Pedido pedido)
+        {
+            List<string> listaErros;
+            if (!pedido.Validar(out listaErros))
+            {
+                return BadRequest(string.Join(".\n", listaErros.ToArray()));
+            }
+            repositorio.Alterar(pedido);
+            return Ok(pedido);
+        }
+    }
+}
