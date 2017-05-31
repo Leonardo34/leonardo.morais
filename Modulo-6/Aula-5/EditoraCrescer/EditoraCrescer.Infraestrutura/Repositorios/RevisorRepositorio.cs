a639e42164bb3d87ca4cf78055d651a7dfdf656f
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-31 01:06:20 -0300

    feat(Modulo-6 EditoraCrescer): Adicionar controller e repositorio de revisores, permitindo retornar revisores cadastrados, incluir novo revisor e deletar revisor existente

---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/RevisorRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+    public class RevisorRepositorio
+    {
+        private Contexto.Contexto revisorContexto = new Contexto.Contexto();
+
+        public List<Revisor> Listar()
+        {
+            return revisorContexto.Revisores.ToList();
+        }
+
+        public void Salvar(Revisor revisor)
+        {
+            revisorContexto.Revisores.Add(revisor);
+        }
+
+        public Revisor Excluir(int id)
+        {
+            var revisorExcluido = revisorContexto.Revisores.FirstOrDefault(r => r.Id == id);
+            revisorContexto.Revisores.Remove(revisorExcluido);
+            return revisorExcluido;
+        }
+    }
+}
---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.WebAPI/Controllers/RevisoresController.cs
@@ -0,0 +1,32 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.WebAPI.Controllers
+{
+    public class RevisoresController : ApiController
+    {
+        private RevisorRepositorio repositorio = new RevisorRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Listar());
+        }
+
+        public IHttpActionResult Post(Revisor revisor)
+        {
+            repositorio.Salvar(revisor);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok(repositorio.Excluir(id));
+        }
+    }
+}
