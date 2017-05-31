b774b065a3ad485fca9a9ac82088f1800078edd5
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-05-31 00:56:22 -0300

    feat(Modulo-6 EditoraCrescer): Criar repositorio para autores, permitindo listar autores cadastrados, salvar novo autor e deletar autor existente

---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+    public class AutorRepositorio
+    {
+        private Contexto.Contexto autorContexto = new Contexto.Contexto();
+
+        private List<Autor> Listar()
+        {
+            return autorContexto.Autores.ToList();
+        }
+
+        private void Salvar(Autor autor)
+        {
+            autorContexto.Autores.Add(autor);
+        }
+
+        public Autor Excluir(int id)
+        {
+            var autorExcluido = autorContexto.Autores.FirstOrDefault(a => a.Id == id);
+            autorContexto.Autores.Remove(autorExcluido);
+            return autorExcluido;
+        }
+    }
+}
