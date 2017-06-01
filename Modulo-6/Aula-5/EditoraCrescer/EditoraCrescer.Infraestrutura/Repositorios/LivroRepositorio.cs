38310188ec6f9c7ff45cb45bc69979b23c59f784
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-01 01:39:04 -0300

    feat(Modulo-6 EditoraCrescer): Permitir busca de livros por genero e pelo isbn

--- Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/LivroRepositorio.cs
@@ -10,28 +10,40 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
 {
     public class LivroRepositorio
     {
-        private Contexto.Contexto livroContexto = new Contexto.Contexto();
+        private Contexto.Contexto contexto = new Contexto.Contexto();
 
         public LivroRepositorio()
         { }
 
         public List<Livro> Listar()
         {
-            return livroContexto.Livros.ToList();
+            return contexto.Livros.ToList();
         }
 
         public void Salvar(Livro livro)
         {
-            livroContexto.Livros.Add(livro);
-            livroContexto.SaveChanges();
+            contexto.Livros.Add(livro);
+            contexto.SaveChanges();
         }
 
         public Livro Excluir(int id)
         {
-            var livroRemovido = livroContexto.Livros.FirstOrDefault(l => l.Isbn == id);
-            livroContexto.Livros.Remove(livroRemovido);
-            livroContexto.SaveChanges();
+            var livroRemovido = contexto.Livros.FirstOrDefault(l => l.Isbn == id);
+            contexto.Livros.Remove(livroRemovido);
+            contexto.SaveChanges();
             return livroRemovido;
+        }
+
+        public Livro ObterPorIsbn(int isbn)
+        {
+            return contexto.Livros.FirstOrDefault(l => l.Isbn == isbn);
+        }
+
+        public List<Livro> ObterPorGenero(string genero)
+        {
+            return contexto.Livros
+                .Where(l => l.Genero.Contains(genero))
+                .ToList();
         }
     }
 }
