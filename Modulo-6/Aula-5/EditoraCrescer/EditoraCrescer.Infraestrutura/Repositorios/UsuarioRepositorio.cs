e197950ac20c46afa8b358ba3e4db4e1873bde9e
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-04 19:39:02 -0300

    feat(Modulo-6 EditoraCrescerApi): Adicionar repositório e controller para operações com usuários do sistema

--- Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/UsuarioRepositorio.cs
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/UsuarioRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,31 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
 {
     public class UsuarioRepositorio
     {
+        private Contexto.Contexto contexto = new Contexto.Contexto();
 
+        public void Salvar(Usuario usuario)
+        {
+            usuario.Permissoes.Add(BuscarPermissao(1));
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+        }
+
+        public Permissao BuscarPermissao(int id)
+        {
+            return contexto.Permissoes.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Alterar(Usuario usuario)
+        {
+            contexto.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public Usuario Obter(string email)
+        {
+            return contexto.Usuarios
+                .Include("Permissoes")
+                .FirstOrDefault(u => u.Email.Equals(email));
+        }
     }
 }
---
+++ Modulo-6/Aula-5/EditoraCrescer/EditoraCrescer.WebAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,94 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using EditoraCrescer.WebAPI.App_Start;
+using EditoraCrescer.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace EditoraCrescer.WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/acessos")]
+    public class UsuariosController : ApiController
+    {
+        private UsuarioRepositorio repositorio = new UsuarioRepositorio();
+
+        [HttpPost, Route("registrar")]
+        public HttpResponseMessage Registrar([FromBody]RegistrarUsuarioModel model)
+        {
+            if (repositorio.Obter(model.Email) == null)
+            {
+                var usuario = new Usuario(model.Nome, model.Email, model.Senha);
+
+                if (usuario.Validar())
+                {
+                    repositorio.Salvar(usuario);
+                }
+                else
+                {
+                    return ResponderErro(usuario.Mensagens);
+                }
+            }
+            else
+            {
+                return ResponderErro("Usuário já existe.");
+            }
+
+            return ResponderOK();
+        }
+
+        [HttpPost, Route("resetarsenha")]
+        public HttpResponseMessage ResetarSenha(string email)
+        {
+            var usuario = repositorio.Obter(email);
+            if (usuario == null)
+                return ResponderErro(new string[] { "Usuário não encontrado." });
+
+            var novaSenha = usuario.ResetarSenha();
+
+            if (usuario.Validar())
+            {
+                repositorio.Alterar(usuario);
+                // EmailService.Enviar(usuario.Email, "Crescer 2017-1", $"Olá! sua senha foi alterada para: {novaSenha}");
+            }
+            else
+                return ResponderErro(usuario.Mensagens);
+
+            return ResponderOK();
+        }
+
+        // Exige que o usuário se autentique
+        [AutenticacaoBasic64]
+        [HttpGet, Route("usuario")]
+        public HttpResponseMessage Obter()
+        {
+            // só pode obter as informações do usuário corrente (logado, autenticado)
+            var usuario = repositorio.Obter(Thread.CurrentPrincipal.Identity.Name);
+
+            if (usuario == null)
+                return ResponderErro("Usuário não encontrado.");
+
+            return ResponderOK(new { usuario.Nome, usuario.Permissoes, usuario.Email });
+        }
+
+        public HttpResponseMessage ResponderOK(object dados = null)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { dados });
+        }
+
+        public HttpResponseMessage ResponderErro(params string[] mensagens)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+        }
+
+        public HttpResponseMessage ResponderErro(IEnumerable<string> mensagens)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+        }
+    }
+}
