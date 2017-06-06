0dc029e09db29a0f012bb9d5d8717288c029d024
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-06 10:16:45 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar repositório para os combos

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/ComboRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using ImobiliariaCrescer.Infraestrutura.ContextoEntity;
+using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public class ComboRepositorio : IRepositorio<Combo>
+    {
+        private Contexto contexto = new Contexto();
+
+        public void Alterar(Combo objeto)
+        {
+            contexto.Entry(objeto).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(Combo objeto)
+        {
+            contexto.Combos.Add(objeto);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var comboDeletado = ObterPorId(id);
+            contexto.Combos.Remove(comboDeletado);
+            contexto.SaveChanges();
+        }
+
+        public List<Combo> Listar()
+        {
+            return contexto.Combos.ToList();
+        }
+
+        public Combo ObterPorId(int id)
+        {
+            return contexto.Combos.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
