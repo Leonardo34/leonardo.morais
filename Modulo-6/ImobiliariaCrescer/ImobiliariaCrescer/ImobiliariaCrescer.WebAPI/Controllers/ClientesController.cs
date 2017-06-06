29ed21cbee0fa0bd946a86d7185c98690e8f1fc6
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-06 10:07:50 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar controller para clientes

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.WebAPI/Controllers/ClientesController.cs
@@ -0,0 +1,65 @@
+﻿using ImobiliariaCrescer.Infraestrutura.Entidades;
+using ImobiliariaCrescer.Infraestrutura.Repositorios;
+using ImobiliariaCrescer.WebAPI.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImobiliariaCrescer.WebAPI.Controllers
+{
+    [RoutePrefix("api/clientes")]
+    public class ClientesController : ApiController
+    {
+        private ClienteRepositorio repositorio = new ClienteRepositorio();
+
+        [AutenticacaoBasic64]
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                new { data = repositorio.Listar() });
+        }
+
+        [AutenticacaoBasic64]
+        [Route("{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            var cliente = repositorio.ObterPorId(id);
+            if (cliente == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound,
+                   new { error = "Não existe cliente com o id informado" });
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, new { data = cliente });
+        }
+
+        [AutenticacaoBasic64]
+        [HttpPost]
+        public IHttpActionResult Post(Cliente cliente)
+        {
+            repositorio.Criar(cliente);
+            return Ok();
+        }
+
+        [AutenticacaoBasic64]
+        [Route("{id:int}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            repositorio.Deletar(id);
+            return Ok();
+        }
+
+        [AutenticacaoBasic64]
+        [HttpPut]
+        public IHttpActionResult Alterar(Cliente cliente)
+        {
+            repositorio.Alterar(cliente);
+            return Ok();
+        }
+    }
+}
