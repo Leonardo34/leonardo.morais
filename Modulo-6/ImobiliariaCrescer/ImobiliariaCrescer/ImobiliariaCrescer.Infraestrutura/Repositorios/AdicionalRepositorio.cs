af408aa742b361e361392348ffb0722d23725b3f
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-06 16:59:51 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar repositorio para adicionais

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/AdicionalRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using ImobiliariaCrescer.Infraestrutura.ContextoEntity;
+using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public class AdicionalRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public void Alterar(Adicional objeto)
+        {
+            contexto.Entry(objeto).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(Adicional objeto)
+        {
+            contexto.Adicionais.Add(objeto);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var adicionalDeletado = ObterPorId(id);
+            contexto.Adicionais.Remove(adicionalDeletado);
+            contexto.SaveChanges();
+        }
+
+        public List<Adicional> Listar()
+        {
+            return contexto.Adicionais.ToList();
+        }
+
+        public Adicional ObterPorId(int id)
+        {
+            return contexto.Adicionais.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
