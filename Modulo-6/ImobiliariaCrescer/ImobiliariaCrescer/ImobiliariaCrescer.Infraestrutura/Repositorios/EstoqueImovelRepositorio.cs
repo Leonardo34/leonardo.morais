a2d8327e87f86d80d6bafd13e6f485280306c933
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-06 15:16:23 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar repositorio para estoques de Imoveis

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/EstoqueImovelRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using ImobiliariaCrescer.Infraestrutura.ContextoEntity;
+using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public class EstoqueImovelRepositorio : IRepositorio<EstoqueImovel>
+    {
+        private Contexto contexto = new Contexto();
+
+        public void Alterar(EstoqueImovel objeto)
+        {
+            contexto.Entry(objeto).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(EstoqueImovel objeto)
+        {
+            contexto.Estoques.Add(objeto);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var estoqueDeletar = ObterPorId(id);
+            contexto.Estoques.Remove(estoqueDeletar);
+            contexto.SaveChanges();
+        }
+
+        public List<EstoqueImovel> Listar()
+        {
+            return contexto.Estoques.ToList();
+        }
+
+        public EstoqueImovel ObterPorId(int id)
+        {
+            return contexto.Estoques.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
