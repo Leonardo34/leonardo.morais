b1e9d4dc64ce17a09835650ae98f8185ea8bae06
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-06 00:32:46 -0300

    feat(Modulo-6 ImobiliariaCrescer): Adicionar repositorio, controller e autenticação para usuários do sistema

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/IRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public interface IRepositorio<T> 
+    {
+        List<T> Listar();
+        T ObterPorId(int id);
+        T Criar(T objeto);
+        T Alterar(T objeto);
+        void Deletar(int id);
+    }
+}
