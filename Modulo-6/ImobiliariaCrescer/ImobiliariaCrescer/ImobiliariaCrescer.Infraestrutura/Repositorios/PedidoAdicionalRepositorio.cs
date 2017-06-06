86ea476ee7fb0ca08b0045056537508774b527eb
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-06 16:35:46 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar repositorio para pedidosAdicionais

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/PedidoAdicionalRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using ImobiliariaCrescer.Infraestrutura.ContextoEntity;
+using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public class PedidoAdicionalRepositorio : IRepositorio<PedidoAdicional>
+    {
+        private Contexto contexto = new Contexto();
+
+        public void Alterar(PedidoAdicional objeto)
+        {
+            contexto.Entry(objeto).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(PedidoAdicional objeto)
+        {
+            contexto.PedidosAdicionais.Add(objeto);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var pedidoAdicionalDeletado = ObterPorId(id);
+            contexto.PedidosAdicionais.Remove(pedidoAdicionalDeletado);
+            contexto.SaveChanges();
+        }
+
+        public List<PedidoAdicional> Listar()
+        {
+            return contexto.PedidosAdicionais.ToList();
+        }
+
+        public PedidoAdicional ObterPorId(int id)
+        {
+            return contexto.PedidosAdicionais.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
