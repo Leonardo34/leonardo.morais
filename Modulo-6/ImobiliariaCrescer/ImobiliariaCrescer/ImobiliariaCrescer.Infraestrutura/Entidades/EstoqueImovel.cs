1bb4fa472090b294ab86491d3edac7da1d0f993c
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-06 14:13:18 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar entidade para controlar estoque de casas disponíveis para aluguel

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Entidades/EstoqueImovel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Entidades
+{
+    public class EstoqueImovel
+    {
+        public int Id { get; set; }
+        public int IdImovel { get; set; }
+        public Imovel Imovel { get; set; }
+        public int IdCombo { get; set; }
+        public Combo Combo { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Mappings/EstoqueImovelMap.cs
@@ -0,0 +1,26 @@
+﻿using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Mappings
+{
+    public class EstoqueImovelMap : EntityTypeConfiguration<EstoqueImovel>
+    {
+        public EstoqueImovelMap()
+        {
+            ToTable("EstoqueImovel");
+
+            HasRequired(x => x.Imovel)
+                .WithMany()
+                .HasForeignKey(x => x.IdImovel);
+
+            HasRequired(x => x.Combo)
+                .WithMany()
+                .HasForeignKey(x => x.IdCombo);
+        }
+    }
+}
