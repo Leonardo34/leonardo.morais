45755db48bf5010318c7c6b7ec94e28022215740
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-06 14:57:17 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Implementar repositório para pedidos

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using ImobiliariaCrescer.Infraestrutura.ContextoEntity;
+using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobiliariaCrescer.Infraestrutura.Repositorios
+{
+    public class PedidoRepositorio : IRepositorio<Pedido>
+    {
+        private Contexto contexto = new Contexto();
+        
+        public void Alterar(Pedido objeto)
+        {
+            contexto.Entry(objeto).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(Pedido objeto)
+        {
+            contexto.Pedidos.Add(objeto);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var pedidoDeletar = ObterPorId(id);
+            contexto.Pedidos.Remove(pedidoDeletar);
+            contexto.SaveChanges();
+        }
+
+        public List<Pedido> Listar()
+        {
+            return contexto.Pedidos.ToList();
+        }
+
+        public Pedido ObterPorId(int id)
+        {
+            return contexto.Pedidos.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
