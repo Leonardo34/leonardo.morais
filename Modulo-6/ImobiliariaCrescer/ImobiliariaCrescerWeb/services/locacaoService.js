1e164dcc3847978be8a34c8f4254781b19a0f618
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-07 16:10:30 -0300

    feat(Modulo-6 ImobiliariaCrescerWeb): Adicionar buscar de imoveis, combos e adicionais na service de locação do angular

--- Modulo-6/ImobiliariaCrescer/ImobiliariaCrescerWeb/services/locacaoService.js
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescerWeb/services/locacaoService.js
@@ -3,4 +3,16 @@ app.service('locacaoService', function($http) {
     this.buscarClientePorCpf = function(cpf) {
         return $http.get('http://localhost:50573/api/clientes?cpf=' + cpf);
     }
+
+    this.buscarImoveis = function() {
+        return $http.get('http://localhost:50573/api/imoveis');
+    }
+
+    this.buscarCombos = function() {
+        return $http.get('http://localhost:50573/api/combos');
+    }
+
+    this.buscarAdicionais = function() {
+        return $http.get('http://localhost:50573/api/adicionais');
+    }
 });
