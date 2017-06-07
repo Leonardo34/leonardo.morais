4c890abe1db303f02fa382bd7c0078a33eaedb11
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-07 15:07:09 -0300

    feat(Modulo-6 ImobiliariaCrescerWeb): Adicionar implementação inicial da página para realização do aluguel e controller da página

--- Modulo-6/ImobiliariaCrescer/ImobiliariaCrescerWeb/alugarImovel/alugarImovel.Controller.js
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescerWeb/alugarImovel/alugarImovel.Controller.js
@@ -1,3 +1,21 @@
-app.controller('alugarImovel.Controller', function($scope, authService, $location, clientesService) {
+app.controller('alugarImovel.Controller', function($scope, authService, $location, locacaoService) {
     $scope.url.path = $location.path();
+    $scope.pedido = {};
+    $scope.step = 1;
+
+    $scope.adicionarCliente = function() {
+        locacaoService.buscarClientePorCpf($scope.cliente.Cpf)
+            .then(res => {
+                $scope.pedido.cliente = res.data.data;
+                $scope.step++;
+            }, error => {
+                window.alert('Erro ao buscar usuario por CPF');
+            });
+    }
+
+    $scope.previous = function() {
+        if ($scope.step > 1) {
+            $scope.step--;
+        }
+    }
 });
