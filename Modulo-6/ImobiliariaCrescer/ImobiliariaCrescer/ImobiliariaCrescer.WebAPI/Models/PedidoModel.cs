03c303fdff51398844c8fcbc5fbe177c41cd06ac
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-08 10:25:43 -0300

    feat(Modulo-6 ImobiliariaCrescerApi): Adicionar model de pedido, que será recebido da aplicação web

---
+++ Modulo-6/ImobiliariaCrescer/ImobiliariaCrescer/ImobiliariaCrescer.WebAPI/Models/PedidoModel.cs
@@ -0,0 +1,17 @@
+﻿using ImobiliariaCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImobiliariaCrescer.WebAPI.Models
+{
+    public class PedidoModel
+    {
+        public List<Adicional> Adicionais { get; set; }
+        public Combo Combo { get; set; }
+        public Imovel Imovel { get; set; }
+        public Cliente Cliente { get; set; }
+        public int DiasAluguel { get; set; }
+    }
+}
