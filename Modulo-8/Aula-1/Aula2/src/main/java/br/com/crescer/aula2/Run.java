cf936549d266cc8022ca0806ec99ce4d9327c0d6
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-21 16:04:56 -0300

    feat(Modulo-8 Aula02): Adicionar método para listar todos arquivos dentro de um diretório com busca recursiva, listando todos arquivos dentro de todas sub-pastas

--- Modulo-8/Aula-1/Aula2/src/main/java/br/com/crescer/aula2/Run.java
+++ Modulo-8/Aula-1/Aula2/src/main/java/br/com/crescer/aula2/Run.java
@@ -1,8 +1,21 @@
 package br.com.crescer.aula2;
 
 import java.io.File;
+import java.util.Arrays;
 public class Run {
     public static void main(String[] args) {
-        final File file = new File("aula2.txt");
+        File file = new File(".");
+        printFiles(file);
+                
+    }
+    
+    private static void printFiles(File file) {
+        for (File each : file.listFiles()) {
+            if (each.isDirectory()) {
+                printFiles(each);
+            } else {
+                System.out.println(each.getAbsolutePath());
+            }
+        }
     }
 }
