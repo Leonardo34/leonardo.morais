abe9b80eff94804d1de5ac245c0ac3a20a061019
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-21 09:57:16 -0300

    feat(Modulo-8 Tema01): Adicionar método para retornar string com tempo decorrido entre uma data e o dia atual

--- Modulo-8/Aula-1/Tema01/Tema01/src/main/java/br/com/crescer2017/tema01/CalendarUtils.java
+++ Modulo-8/Aula-1/Tema01/Tema01/src/main/java/br/com/crescer2017/tema01/CalendarUtils.java
@@ -14,8 +14,20 @@ public class CalendarUtils implements ICalendarUtils {
     
     @Override
     public String tempoDecorrido(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        return null;
+        Calendar agora = Calendar.getInstance();
+        Calendar data = Calendar.getInstance();
+        data.setTime(date);
+        int anos = agora.get(Calendar.YEAR) - data.get(Calendar.YEAR);
+        int meses = agora.get(Calendar.MONTH) - data.get(Calendar.MONTH);
+        int dias = agora.get(Calendar.DAY_OF_MONTH) - data.get(Calendar.DAY_OF_MONTH);
+        if (dias < 0) {
+            dias += 30;
+            meses--;
+        }
+        if (meses < 0) {
+            meses += 12;
+            anos--;
+        }
+        return String.format("%d ano(s), %d mes(es) e %d dia(s)", anos, meses, dias);
     }
 }
