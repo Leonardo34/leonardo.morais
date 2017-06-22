322e179e5c3fd644065f16a40e41f13e66060b28
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-22 00:42:57 -0300

    feat(Modulo-8 Tema2): Impedir leitura de arquivos que não possuam a extensão .txt na classe ReaderUtils

--- Modulo-8/Aula-1/Tema02/src/main/java/br/com/crescer2017/tema02/IReaderUtils.java
+++ Modulo-8/Aula-1/Tema02/src/main/java/br/com/crescer2017/tema02/IReaderUtils.java
@@ -1,7 +1,5 @@
 package br.com.crescer2017.tema02;
 
-import java.io.IOException;
-
 public interface IReaderUtils {
-    String read(String string) throws IOException;
+    String read(String string) throws Exception;
 }
--- Modulo-8/Aula-1/Tema02/src/main/java/br/com/crescer2017/tema02/ReaderUtils.java
+++ Modulo-8/Aula-1/Tema02/src/main/java/br/com/crescer2017/tema02/ReaderUtils.java
@@ -1,13 +1,18 @@
 package br.com.crescer2017.tema02;
 
+import br.com.crescer2017.tema01.StringUtils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReaderUtils implements IReaderUtils {
+    private final StringUtils stringUtils = new StringUtils();
 
     @Override
-    public String read(String string) throws IOException {
+    public String read(String string) throws Exception {
+        if (!stringUtils.getFileExtension(string).equals("txt")) {
+            throw new Exception("O Arquivo lido deve ser do tipo txt");
+        }
         StringBuilder conteudo = new StringBuilder();
         try (BufferedReader reader = 
                 new BufferedReader(new FileReader(string))) {
