a366cc8341d985b0af3b6ff43c21e7ff5d721254
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-23 00:25:02 -0300

    fix(Modulo-8 Tema3): Atualizar usuario e senha do ConnectionUtils

--- Modulo-8/Aula-1/Aula3/src/main/java/br/com/crescer/aula3/ConnectionUtils.java
+++ Modulo-8/Aula-1/Aula3/src/main/java/br/com/crescer/aula3/ConnectionUtils.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class ConnectionUtils {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USER = "crescer";
-    private static final String PASS = "crescer";
+    private static final String USER = "system";
+    private static final String PASS = "oracle";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
