2566d1720c09f86b377f7ecfd11f219fa3946651
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-24 14:44:28 -0300

    feat(Modulo-8 Tema4): Adicionar mapeamento da classe Locacao

---
+++ Modulo-8/Aula-1/Tema4/src/main/java/br/com/crescer2017/tema4/models/Locacao.java
@@ -0,0 +1,95 @@
+package br.com.crescer2017.tema4.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "FUNCIONARIO")
+public class Locacao {
+    @Id 
+    @Basic(optional = false)
+    @Column(name = "ID")
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_LOCACAO")
+    @SequenceGenerator(name = "SEQ_LOCACAO", sequenceName = "SEQ_LOCACAO", allocationSize=1)
+    private Long id;
+    
+    @Column(name = "VALOR_TOTAL")
+    private BigDecimal valorTotal;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DATA_DEVOLUCAO")
+    private Date dataDevolucao;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ID_FUNCIONARIO")
+    private Funcionario funcionario;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ID_CLIENTE")
+    private Cliente cliente;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ID_VIDEO")
+    private Video video;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+}
