937df077d18ae3a95696591218992670b3217431
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-24 12:51:42 -0300

    feat(Modulo-8 Tema4): Adicionar mapeamento da classe Cliente e ClienteDao

---
+++ Modulo-8/Aula-1/Tema4/src/main/java/br/com/crescer2017/tema4/models/repository/ClienteDao.java
@@ -0,0 +1,9 @@
+package br.com.crescer2017.tema4.models.repository;
+
+import br.com.crescer2017.tema4.models.Cliente;
+
+public class ClienteDao extends GenericDao<Cliente, Long> {
+    public ClienteDao() {
+        super(Cliente.class);
+    }
+}
