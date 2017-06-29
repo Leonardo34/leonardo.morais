311462fd8de27b8f4c8798779fd22c1e694937fc
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-28 21:14:25 -0300

    feat(Modulo-8 Tema-Spring): Adicionar service de Locações

---
+++ Modulo-8/Aula-1/Aula7/src/main/java/br/com/crescer/aula7/services/LocacaoService.java
@@ -0,0 +1,27 @@
+package br.com.crescer.aula7.services;
+
+import br.com.crescer.aula7.models.Locacao;
+import br.com.crescer.aula7.repositorys.LocacaoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LocacaoService {
+    @Autowired
+    private LocacaoRepository locacaoRepositorio;
+    
+    public List<Locacao> list() {
+        return (List) locacaoRepositorio.findAll();
+    }
+    
+    public Locacao addGenero(Locacao locacao) {
+        return locacaoRepositorio.save(locacao);
+    }
+    
+    public void remove(Locacao locacao) {
+        locacaoRepositorio.delete(locacao);
+    }
+    
+    public Locacao getGeneroById(Long id) {
+        return locacaoRepositorio.findOne(id);
+    }
+}
