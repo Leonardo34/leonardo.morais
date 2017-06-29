96bd1d39b3007a73cd757639d762c6b78f349b24
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-28 21:04:18 -0300

    feat(Modulo-8 Tema-Spring): Adicionar service de Cliente

---
+++ Modulo-8/Aula-1/Aula7/src/main/java/br/com/crescer/aula7/services/ClienteService.java
@@ -0,0 +1,27 @@
+package br.com.crescer.aula7.services;
+
+import br.com.crescer.aula7.models.Cliente;
+import br.com.crescer.aula7.repositorys.ClienteRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepositorio;
+    
+    public List<Cliente> list() {
+        return (List) clienteRepositorio.findAll();
+    }
+    
+    public Cliente addGenero(Cliente cliente) {
+        return clienteRepositorio.save(cliente);
+    }
+    
+    public void remove(Cliente cliente) {
+        clienteRepositorio.delete(cliente);
+    }
+    
+    public Cliente getGeneroById(Long id) {
+        return clienteRepositorio.findOne(id);
+    }
+}
