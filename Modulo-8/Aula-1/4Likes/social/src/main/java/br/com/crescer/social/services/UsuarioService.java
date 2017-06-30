7eda01453dbf399d153335e639e916766a6a0dc0
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-30 00:50:37 -0300

    feat(Modulo-8 4Likes): Adicionar service para usuarios

---
+++ Modulo-8/Aula-1/4Likes/social/src/main/java/br/com/crescer/social/services/UsuarioService.java
@@ -0,0 +1,45 @@
+package br.com.crescer.social.services;
+
+import br.com.crescer.social.models.Usuario;
+import br.com.crescer.social.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepositorio;
+    
+    public Page<Usuario> findAll(Pageable pageable) {
+        return usuarioRepositorio.findAll(pageable);
+    }
+
+    public Iterable<Usuario> findAll() {
+        return usuarioRepositorio.findAll();
+    }
+
+    public void delete(Long id) {
+        usuarioRepositorio.delete(id);
+    }
+
+    public Usuario findById(Long id) {
+        return usuarioRepositorio.findOne(id);
+    }
+
+    public Usuario findByEmail(String email) {
+        return usuarioRepositorio.findByEmail(email);
+    }
+
+    public Usuario save(Usuario usuario) {
+        usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+        return usuarioRepositorio.save(usuario);
+    }
+
+    public Usuario update(Usuario usuario) {
+        return usuarioRepositorio.save(usuario);
+    }
+}
