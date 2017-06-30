8cbf67d73adfedce78b75ee4ce580a88f12d435f
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-06-30 17:59:32 -0300

    fix(Modulo-8 4Likes): Corrigir nome de tabela que entrava em conflito com palavra reservada do Oracle

--- Modulo-8/Aula-1/4Likes/social/src/main/java/br/com/crescer/social/models/Like.java
+++ Modulo-8/Aula-1/4Likes/social/src/main/java/br/com/crescer/social/models/Like.java
@@ -1,5 +1,6 @@
 package br.com.crescer.social.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,19 +10,22 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Curtida")
 public class Like implements Serializable {
     @Id
     @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_LIKE")
     @SequenceGenerator(name = "SEQ_LIKE", sequenceName = "SEQ_LIKE", allocationSize = 1)
-    @Column(name = "ID_LIKE")
+    @Column(name = "ID_CURTIDA")
     private Long id;
     
     @ManyToOne
     @JoinColumn(name = "ID_USUARIO_CURTIDA")
     private Usuario usuarioCurtida;
     
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "ID_POST")
     private Post post;
