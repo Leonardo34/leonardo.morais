7cdf00e0a556fc70ae584f8f608a98409c701a35
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-06-30 02:24:43 -0300

    feat(Modulo-8 4Likes): Permitir buscar posts de um usuario através do seu Id

--- Modulo-8/Aula-1/4Likes/social/src/main/java/br/com/crescer/social/controller/PostController.java
+++ Modulo-8/Aula-1/4Likes/social/src/main/java/br/com/crescer/social/controller/PostController.java
@@ -4,9 +4,12 @@ import br.com.crescer.social.models.Post;
 import br.com.crescer.social.models.Usuario;
 import br.com.crescer.social.services.PostService;
 import br.com.crescer.social.services.UsuarioService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +29,10 @@ public class PostController {
         post.setUsuario(usuarioLogado);
         postService.save(post);
     }
+    
+    @GetMapping(value = "posts/{id}")
+    public List<Post> getPostsUser(@PathVariable Long id) {
+        return usuarioService.findById(id).getPosts();
+    }
+    
 }
