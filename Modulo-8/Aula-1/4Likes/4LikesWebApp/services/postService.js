a703b328128f2561ebc0b02af54ef734ad5cb421
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-07-01 22:32:26 -0300

    feat(Modulo-8 4LikesWebApp): Adicionar service para curtir e descurtir posts e adicionar comentarios a um post

--- Modulo-8/Aula-1/4Likes/4LikesWebApp/services/postService.js
+++ Modulo-8/Aula-1/4Likes/4LikesWebApp/services/postService.js
@@ -3,4 +3,16 @@ app.service('postService', function($http) {
     this.getPostsFeed = function() {
         return $http.get('http://localhost:9090/posts/feed');
     }
+
+    this.curtirPost = function(idPost) {
+        return $http.post('http://localhost:9090/like/' + idPost, {});
+    }
+
+    this.descurtirPost = function(idLike) {
+        return $http.post('http://localhost:9090/deslike/' + idLike, {});
+    }
+
+    this.enviarComentario = function(comentario, post) {
+        return $http.post('http://localhost:9090/comentario/' + post.id, comentario);
+    }
 });
