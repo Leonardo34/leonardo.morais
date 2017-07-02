94c362bd6cdf5f80fe980f1bba0772a04b8609c6
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-07-01 22:38:20 -0300

    feat(Modulo-8 4LikesWebApp): Possibilitar que usuarios possam curtir e comentar posts que estão no feed

--- Modulo-8/Aula-1/4Likes/4LikesWebApp/feed/feed.controller.js
+++ Modulo-8/Aula-1/4Likes/4LikesWebApp/feed/feed.controller.js
@@ -9,4 +9,33 @@ app.controller('feedController', function($scope, authService, postService) {
             window.alert("Erro");
         })
     }
+
+    $scope.vai = function() {
+        console.log("vai")
+    }
+
+    $scope.postCurtido = function(post) {
+        let usuario = authService.getUsuario();
+        return post.likes.some(l => l.usuarioCurtida.id = usuario.id);
+    }
+
+    $scope.likePost = function(post) {
+        if ($scope.postCurtido(post)) {
+            let idLike = getIdLikeUsuarioLogado(post);
+            postService.descurtirPost(idLike).then(() => carregarPostsFeed());
+        } else {
+            postService.curtirPost(post.id).then(() => carregarPostsFeed());
+        }
+    }
+
+    $scope.adicionarComentario = function(comentario, post) {
+        console.log(comentario);
+        postService.enviarComentario(comentario, post).then(() => carregarPostsFeed());
+    }
+
+    function getIdLikeUsuarioLogado(post) {
+        let usuario = authService.getUsuario();
+        let like = post.likes.find(l => l.usuarioCurtida.id == usuario.id);
+        return like.id;
+    }
 });
